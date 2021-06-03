a965fe769590c8e6d354f4af32ab870c4943deff
AlexSuarez9521 <54213344+AlexSuarez9521@users.noreply.github.com>
2021-06-02 20:43:42 -0500

    ssss

--- GranjaBurger/Express/src/routes/pedidos.js
+++ GranjaBurger/Express/src/routes/pedidos.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const pedidosController = require('../controllers/pedidosController')
 
+/* ff */
 router.get('/:id', pedidosController.ListarCartaPorCategoria)
 
 module.exports = router
